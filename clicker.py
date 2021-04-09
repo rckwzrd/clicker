a2ec2e4e46fdbbe09b21297512a448254847d7e5
mtt rhoads <mrhoads1@gmail.com>
2021-04-09 09:58:49 -0500

    added print statement

--- clicker.py
+++ clicker.py
@@ -1,10 +1,11 @@
 import pyautogui
 
+clicks = 10
 
 try:
     X, Y = pyautogui.locateCenterOnScreen("gen_1000_samples.png")
-    pyautogui.click(X, Y, clicks=10, interval=0.25)
-    print("")
+    pyautogui.click(X, Y, clicks=clicks, interval=0.25)
+    print(f"{clicks*1000} samples generated!")
 
 except Exception:
     print("Button not found. Adjust Statkey screen.")
